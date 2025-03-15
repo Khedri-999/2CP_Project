e5903f2d70fe0147ea80bed3d66dfd15dca7b15c
ghacene <haceneghersi222@gmail.com>
2025-03-15 22:54:03 +0100

    first

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -10,6 +10,9 @@ function LandingPage() {
         <div className="nav-links">
           <button className="nav-btn">About</button>
           <button className="nav-btn">Contact</button>
+          <button className="how-btn">how</button>
+          <button className="why-btn">why</button>
+          <button className="FAQ-btn">FAQ</button>
           <button className="signin-btn">Sign In</button>
         </div>
       </nav>
@@ -26,11 +29,96 @@ function LandingPage() {
 
       <hr></hr>
       
-      <div>
-        <p>
-          hhhh
-        </p>
-      </div>
+      <div className="how-toUse">
+        <h3>How To Use Our Platform</h3>
+         <div className="how-steps">
+           <div>
+            <div>
+                <img src=''/>
+                <h3>access your account</h3>
+              </div>
+              <p>
+                log in whith your @estin.dz , then add your phone number to make it easier for others to reach you.
+              </p>
+           </div>
+
+           <div>
+            <div>
+                <img src=''/>
+                <h3>access your account</h3>
+              </div>
+              <p>
+                log in whith your @estin.dz , then add your phone number to make it easier for others to reach you.
+              </p>
+           </div>
+
+           <div>
+            <div>
+                <img src=''/>
+                <h3>access your account</h3>
+              </div>
+              <p>
+                log in whith your @estin.dz , then add your phone number to make it easier for others to reach you.
+              </p>
+           </div>
+
+           <div>
+            <div>
+                <img src=''/>
+                <h3>access your account</h3>
+              </div>
+              <p>
+                log in whith your @estin.dz , then add your phone number to make it easier for others to reach you.
+              </p>
+           </div>
+
+           <div>
+            <div>
+                <img src=''/>
+                <h3>access your account</h3>
+              </div>
+              <p>
+                log in whith your @estin.dz , then add your phone number to make it easier for others to reach you.
+              </p>
+           </div>
+
+           <div>
+            <div>
+                <img src=''/>
+                <h3>access your account</h3>
+              </div>
+              <p>
+                log in whith your @estin.dz , then add your phone number to make it easier for others to reach you.
+              </p>
+           </div>
+
+           <div>
+            <div>
+                <img src=''/>
+                <h3>access your account</h3>
+              </div>
+              <p>
+                log in whith your @estin.dz , then add your phone number to make it easier for others to reach you.
+              </p>
+           </div>
+
+           <div>
+            <div>
+                <img src=''/>
+                <h3>access your account</h3>
+              </div>
+              <p>
+                log in whith your @estin.dz , then add your phone number to make it easier for others to reach you.
+              </p>
+           </div>
+         </div>
+       </div>
+
+       <div>
+          <div>
+            
+          </div>
+       </div>
     </div>
   );
 }

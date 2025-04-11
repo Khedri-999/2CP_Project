160c65088cc4ae74ec4c5ca01e8fcd0fe4a82ab3
Aziz Hassani <99550449+Azizhassani@users.noreply.github.com>
2025-04-11 17:21:07 +0100

    Delete aziz_backend directory

--- aziz_backend/pollster/foundit/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Founditem,User,LostItem
-# Register your models here.
-
-admin.site.register(Founditem)
-admin.site.register(LostItem)
-admin.site.register(User)
